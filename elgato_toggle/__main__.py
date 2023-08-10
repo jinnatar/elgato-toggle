fc1044ba39f53ed65d4276e59be51a268c89260b
Jinna Kiisuo <jinna+git@nocturnal.fi>
2023-08-10 14:19:37 +0300

    Add simple README.md, run black & flake8

--- elgato_toggle/__main__.py
+++ elgato_toggle/__main__.py
@@ -1,23 +1,24 @@
 import asyncio
-from typing import Optional
 from absl import app, logging, flags
 
-from elgato import Elgato, State, Info
+from elgato import Elgato, State
 from elgato.exceptions import ElgatoConnectionError
 
-FLAGS=flags.FLAGS
+FLAGS = flags.FLAGS
 
-_LIGHTS = flags.DEFINE_multi_string('light', None, "Light to toggle. Repeat for multiple.")
+_LIGHTS = flags.DEFINE_multi_string(
+    "light", None, "Light to toggle. Repeat for multiple."
+)
+
 
 async def main():
     """Toggle lights."""
     lights: list = _LIGHTS.value
     if lights:
-        await asyncio.gather(
-            *[ toggle(light ) for light in lights ]
-        )
+        await asyncio.gather(*[toggle(light) for light in lights])
     else:
-        logging.error('No lights provided, use the repeated --light flag to provide hostnames.')
+        logging.error("No lights provided, use --light flag(s) to provide hostnames.")
+
 
 async def toggle(hostname: str) -> None:
     """Toggle a single light."""
@@ -27,14 +28,17 @@ async def toggle(hostname: str) -> None:
             state: State = await elgato.state()
             await elgato.light(on=(not state.on))
         except ElgatoConnectionError as e:
-            logging.error(f'{e}: {hostname}')
+            logging.error(f"{e}: {hostname}")
+
 
 def run_async(argv):
     del argv
     asyncio.run(main())
 
+
 def run():
     app.run(run_async)
+
 
 if __name__ == "__main__":
     run()
